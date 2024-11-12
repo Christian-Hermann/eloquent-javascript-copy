0fa54243f6430ba0d85ce02c637e9afb1837cebd
Christian Hermann <christianghermann@gmail.com>
2024-11-12 01:24:15 +0000

    something in quotes

--- chapter-02/exercises.js
+++ chapter-02/exercises.js
@@ -26,7 +26,15 @@ LOGS =>
 
 */
 
-function triangles() {
+function triangles(j) {
+
+  let string = '';
+
+  for (let i = 1; i <= j; i++){
+  string += '#'
+
+  console.log(string);
+}
   
 }
 
@@ -48,7 +56,19 @@ on the number:
 */
 
 function fizzBuzz(start, end) {
-  
+  for (var i = start; i <= end; i++){
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    }
+    else if (i % 5 === 0){
+      console.log("buzz");
+    }
+    else if (i % 3 === 0){
+      console.log("fizz");
+    } else {
+      console.log(i);
+    }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
