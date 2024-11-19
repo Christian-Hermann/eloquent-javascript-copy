ec31c11d2f73c5a40ca10eb08a5225ea1b032956
Christian-Hermann <christianghermann@gmail.com>
2024-11-18 21:34:56 -0600

    range

--- chapter-04/exercises.js
+++ chapter-04/exercises.js
@@ -3,18 +3,23 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 function range(start, end, step = 1) {
- let array = [];
 
- for (let i = start; i <= end; i += step) {
-    array.push(i);
- }
- for(let i = start; i >= end; i += step){
+if (start === end){
+  return [];
+}
+var array = [];
+
+if (step > 0){
+  for (let i = start; i <= end; i += step){
     array.push(i);
   }
+} else if (step < 0){
+  for (let i = start; i >= end; i += step){
+    array.push(i)
+  }
 }
-return array;
+return array
 }
-
 ////////////////////////////////////////////////////////////////////////////////
 // sum /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
